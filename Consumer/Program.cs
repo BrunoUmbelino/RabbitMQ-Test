0d9b5e4f8f4380bdfbfeb7c57bf83f22b2ec47b7
Bruno Umbelino <bruno.umbelino0@gmail.com>
2024-02-26 16:01:46 -0300

    first

---
+++ Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "FilaDePensamentos",
+                   durable: false,
+                   exclusive: false,
+                   autoDelete: false,
+                   arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"[x] Mensagem recebida: \n{message}");
+                };
+
+                channel.BasicConsume(queue: "FilaDePensamentos",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "FilaDePensamentos",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                string message = "O urgente geralmente atenta contra o necessário. - Mao Tse-Tung";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "FilaDePensamentos",
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine($"[x] Mensagem enviada: \n{message}");
+            }
+        }
+    }
+}
